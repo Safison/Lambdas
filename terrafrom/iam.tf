255d8af06c4f627464f136faee2a84110aab90bb
svet-g <svetlana.gljadelkina@gmail.com>
2025-02-04 17:15:19 +0000

    redo the iam structure and policies for s3 and cloudwatch

--- terrafrom/iam.tf
+++ terrafrom/iam.tf
@@ -1,46 +1,55 @@
-
-
-data "aws_iam_policy_document" "lamda_asume_role" {
-  
+data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
+
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
+
     actions = ["sts:AssumeRole"]
   }
+}
 
+resource "aws_iam_role" "iam-for-lambda-role" {
+  name               = "iam-for-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "s3-policy" {
   statement {
-    
+    sid = "1"
     actions = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.random-bucket.arn}/*"]
+    resources = ["${aws_s3_bucket.random-bucket.arn}/*", ]
   }
+} 
 
+resource "aws_iam_policy" "s3-policy" {
+  name        = "s3-policy"
+  policy      = data.aws_iam_policy_document.s3-policy.json
+}
+
+resource "aws_iam_policy_attachment" "s3-attach" {
+  name       = "s3-attachment"
+  roles      = [aws_iam_role.iam-for-lambda-role.name]
+  policy_arn = aws_iam_policy.s3-policy.arn
+}
+
+data "aws_iam_policy_document" "cloudwatch-policy" {
   statement {
     effect = "Allow"
     actions = ["logs:*"]
     resources = ["*"]
-  }
-  
+  } 
+} 
 
+resource "aws_iam_policy" "cloudwatch-policy" {
+  name        = "cloudwatch-policy"
+  policy      = data.aws_iam_policy_document.cloudwatch-policy.json
 }
 
- 
-resource "aws_iam_role" "monster_role" {
-    name = "monster_iam_role"
-    assume_role_policy = data.aws_iam_policy_document.lamda_asume_role.json
-
+resource "aws_iam_policy_attachment" "cloudwatch-attach" {
+  name       = "cloudwatch-attachment"
+  roles      = [aws_iam_role.iam-for-lambda-role.name]
+  policy_arn = aws_iam_policy.cloudwatch-policy.arn
 }
-
-resource "aws_iam_policy" "s3_role_policy" {
-    name = "s3_policy"
-    policy = data.aws_iam_policy_document.lamda_asume_role.json
-
-}
-
-# resource "aws_iam_policy "s3_policy" {
-#   name = "s3_policy"
-#   policy = data.aws_iam_policy_document.lamda_asume_role.json
-# }
-
