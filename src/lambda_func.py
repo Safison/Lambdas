1522ee3390f5243ca0105da36d2ca3491a7e1bd9
svet-g <svetlana.gljadelkina@gmail.com>
2025-02-04 17:05:08 +0000

    change bucket name as was taken since last destroy

--- src/lambda_func.py
+++ src/lambda_func.py
@@ -17,7 +17,7 @@ def lambda_handler(event, context):
     result = str(result)
     result_encoded=result.encode('utf-8')
 
-    bucket_name = "random-bucket-12345"
+    bucket_name = "la-random-bucket-12345"
     file_name = f"sprintrandomfile{datetime.now()}.txt"
     s3 = boto3.resource('s3')
     s3.Bucket(bucket_name).put_object(Key=file_name,Body=result_encoded)
